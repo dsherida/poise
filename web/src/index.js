850beb8f55341a5a2931c1a0994c8f4d058b4008
Doug Sheridan <dsherida@asu.edu>
2018-05-13 00:20:00 -0700

    Integrate the Firebase API into /mobile and /web

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as firebase from "firebase";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAAH82uDeXtWdLJe5OjCgcthKXIn5WhFWU",
+  authDomain: "poise-firebase.firebaseapp.com",
+  databaseURL: "https://poise-firebase.firebaseio.com",
+  storageBucket: "poise-firebase.appspot.com"
+};
+firebase.initializeApp(config);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
